58aa967080bcc718d9c6544c09ef14bba419e1f5
Bhuppi8 <bhuppi.singh8@gmail.com>
2026-09-24 14:39:56 +0530

    register TMDB API & create an app & get access token

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmODY1MzI3MTJkODg4MzI0OTYzMjdmZGU1Y2EwMmMxYSIsIm5iZiI6MTc5MDI0MDA0OC4zOTkwMDAyLCJzdWIiOiI2YWI0ZTUzMDMzMzJmMDU2YjgwNDU1NzEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.HdE7UfaYaA9mk6JRG444JJy5xq7JA-AgJ-Fl1ihDPtA'
+  }
+};
+
